c3163f8bba709e438fd3258807209d6a743249ae
div-tom1506 <divyanshuk1506@gmail.com>
2025-06-04 14:35:40 +0530

    update and delete video method implemented

--- youtube_manager.py
+++ youtube_manager.py
@@ -2,31 +2,51 @@ import json
 
 def load_data():
     try:
-        with open("youtube.txt", "r") as file:
-            return json.load(file)
+        with open('youtube.txt', 'r') as file:
+            load_data = json.load(file)
+            print(load_data)
+            return load_data
     except FileNotFoundError:                    # similar to catch
         return []
     
 def save_data_helper(videos):
-    with open("youtube.txt", "w") as file:
+    with open('youtube.txt', 'w') as file:
         json.dump(videos, file)
 
 
 def list_all_videos(videos):
+    print("\n")
+    print("*" * 70)
     for index, video in enumerate(videos, start=1):
-        print(f"{index}")
+        print(f"{index}. name: {video['name']}, duration: {video['time']}")
+    print("\n")
+    print("*" * 70)
 
 def add_video(videos):
     name = input("Enter video name: ")
     time = input("Enter video time: ")
-    videos.append({"name": name}, {"time": time})
+    videos.append({"name": name, "time": time})
     save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be update: "))
+    if 1 <= index <= len(videos):
+        name = input("Enter new video name: ")
+        time = input("Enter duration of new video: ")
+        videos[index-1] = {'name': name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted: "))
+    if 1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 
 def main():
     videos = load_data()
@@ -38,7 +58,6 @@ def main():
         print("4. Delete youtube video")
         print("5. Exit the app")
         choice = input("Enter your choice: ")
-        print("Videos", videos)
 
         match choice:
             case "1": 
@@ -61,4 +80,3 @@ def main():
         
 if __name__ == "__main__":
     main()
-
